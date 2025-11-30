26656ed6b729619f47d934ddfb112657d8175aaa
mapleDynamicX <3697131063@qq.com>
2025-11-30 17:47:49 +0800

    22

---
+++ merge_ws2/src/merge/src/merge.cpp
@@ -0,0 +1,13 @@
+#include "serial.cpp"
+#include "openvino.cpp"
+#include "threadpool.cpp"
+#include "./livox_ros_driver2/src/livox_ros_driver.h"
+#include "./point_lio/src/laserMapping2.h"
+
+
+
+int main()
+{
+
+    return 0;
+}
