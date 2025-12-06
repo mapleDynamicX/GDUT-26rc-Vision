1d5079ef23ac7ead8975202affcc7a0f8480c82d
mapleDynamicX <3697131063@qq.com>
2025-12-06 17:19:26 +0800

    5

---
+++ merge_ws5/src/merge/src/merge.cpp
@@ -0,0 +1,23 @@
+#include "serial.cpp"
+#include "openvino.cpp"
+#include "threadpool.cpp"
+#include "./livox_ros_driver2/src/livox_ros_driver.h"
+#include "./point_lio/src/laserMapping2.h"
+#include "lidar.cpp"
+#include "camera.cpp"
+#include "test.cpp"
+
+
+
+int main(int argc, char **argv)
+{
+    if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) {
+    ros::console::notifyLoggerLevelsChanged();
+    }
+
+    ros::init(argc, argv, "merge_node");
+    ros::NodeHandle nh;
+
+    test6();
+    return 0;
+}
