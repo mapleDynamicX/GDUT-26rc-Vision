df677a0c0858436049acda09d5b3e420f3361a23
mapleDynamicX <3697131063@qq.com>
2025-12-27 13:58:41 +0800

    16

---
+++ merge_ws16/src/merge/src/merge_func.cpp
@@ -0,0 +1,255 @@
+#ifndef __MERGE_FUNC_CPP_
+#define __MERGE_FUNC_CPP_
+
+
+
+
+#include "serial.h"
+#include "openvino.h"
+#include "threadpool.cpp"
+#include "./livox_ros_driver2/src/livox_ros_driver.h"
+#include "./point_lio/src/laserMapping2.h"
+#include "lidar.h"
+#include "camera.h"
+#include "test.cpp"
+#include "test2.cpp"
+#include "zbuffer_package.cpp"
+#include "relocation.cpp"
+#include "coordinate.h"
+
+
+void yolo()
+{
+    urcu_memb_register_thread();
+    ros::NodeHandle nh("~");
+    image_transport::ImageTransport it(nh);
+    image_transport::Publisher debug_roi_pub = it.advertise("pub_debug_roi",2);
+    image_transport::Publisher zbuffer_pub = it.advertise("pub_image_topic", 2);
+
+    Ten::Ten_zbuffer zbuffer_handler;
+    Ten::Ten_camera& camera =  Ten::Ten_camera::GetInstance();
+
+
+    int arr[31] = {1, 10, 11, 12, 13, 14, 15, 16, 17,18, 19, 2 ,20 ,21 ,22 ,23 ,24, 25 ,26 ,27 ,28 ,29, 3 ,30, 31, 4 ,5 ,6, 7, 8 ,9};
+    std::vector<int> map;
+    for(int i = 0; i < 31; i++)
+    {
+        map.push_back(arr[i]);
+    }    
+    Ten::Ten_yolo_cls detector("/home/maple/study2/merge_ws11/src/merge/model/yolo11-cls_gazebo/best", map);
+    Ten::Ten_map map_s;
+
+    ros::Rate sl(10);
+    while(ros::ok())
+    {
+        // Ten::XYZRPY tf = Ten::Nav_Odometrytoxyzrpy(odo);
+        // std::cout<< "xyz: "<<tf._xyz._x << " " << tf._xyz._y << " " << tf._xyz._z << std::endl;
+        // std::cout<< "rpy: "<<tf._rpy._roll << " " << tf._rpy._pitch << " " << tf._rpy._yaw << std::endl;
+
+        cv::Mat img = camera.camera_read();
+        sl.sleep();
+        nav_msgs::Odometry odo = Ten::_TF_GET_.read_data();
+
+        std::unordered_map<int, cv::Mat> best_roi_image_ = zbuffer_handler.manage_odom_zbuffer_roi(odo, img);
+        std::cout << "std::unordered_map<int, cv::Mat> best_roi_image_.size(): " << best_roi_image_.size() << std::endl;
+        for (auto& x: best_roi_image_)
+        {
+            std::vector<Ten::Detection> results = detector.worker(x.second);
+            if(results.empty())
+            {
+                continue;;
+            }
+            std::cout<< "x.first: " << x.first << "results[0].conf_: " << results[0].conf_ << std::endl;
+            if(map_s.object_confidence_[x.first - 1] < results[0].conf_)
+            {
+                map_s.object_[x.first - 1] = results[0].cls_id_;
+                map_s.object_confidence_[x.first - 1] = results[0].conf_;
+                std::cout<< "map.object_[x.first]" << map_s.object_[x.first - 1] <<std::endl;
+            }
+        }
+        cv::Mat debug = Ten::roi_best_zbuffer_debug2(best_roi_image_, map_s.object_);
+        sensor_msgs::ImagePtr pub_debug_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", debug).toImageMsg();
+        debug_roi_pub.publish(pub_debug_msg);
+        // sensor_msgs::ImagePtr pub_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", img).toImageMsg();
+        // zbuffer_pub.publish(pub_msg);
+
+        
+    }
+
+    camera.~Ten_camera();
+    urcu_memb_unregister_thread();
+
+}
+
+
+
+
+void serial_send_lidar()
+{
+    urcu_memb_register_thread();
+    Ten::Ten_serial& serial = Ten::Ten_serial::GetInstance();
+    float arr[9] = {0};
+
+    Ten::XYZRPY xyzrpy_error;
+    xyzrpy_error._xyz._x = -0.00213777;
+    xyzrpy_error._xyz._y = -0.000782679;
+    xyzrpy_error._xyz._z = -0.00067288;
+    xyzrpy_error._rpy._roll = -0.00503418;
+    xyzrpy_error._rpy._pitch = 0.0161135;
+    xyzrpy_error._rpy._yaw = 0.00025644;
+    Ten::_COORDINATE_TRANSFORMATION_.set_stead_state_error(xyzrpy_error);
+
+    Ten::XYZRPY xyzrpy_car;
+    xyzrpy_car._xyz._x = -0.40944;
+    xyzrpy_car._xyz._y = -0.40944;
+    xyzrpy_car._xyz._z = 0;
+    xyzrpy_car._rpy._roll = 0;
+    xyzrpy_car._rpy._pitch = 0;
+    xyzrpy_car._rpy._yaw = -M_PI / 2.0;
+    Ten::_COORDINATE_TRANSFORMATION_.set_stead_state_error(xyzrpy_car);    
+
+
+    ros::Rate sl(100);
+    while(ros::ok())
+    {
+        nav_msgs::Odometry odo = Ten::_TF_GET_.read_data();
+        Ten::XYZRPY pose = Ten::Nav_Odometrytoxyzrpy(odo);
+        Ten::_COORDINATE_TRANSFORMATION_.set_worldtolidar(pose);
+        Ten::XYZRPY result = Ten::_COORDINATE_TRANSFORMATION_.getXYZRPY();
+
+        float roll = result._rpy._roll;
+        float pitch = result._rpy._pitch;
+        float yaw = result._rpy._yaw;
+
+        arr[0] = result._xyz._x;
+        arr[1] = result._xyz._y;
+        arr[2] = result._xyz._z;
+
+        arr[3] = roll * 180.0 / M_PI;
+        arr[4] = pitch * 180.0 / M_PI;
+        arr[5] = yaw * 180.0 / M_PI;
+
+        arr[6] = odo.twist.twist.linear.x;
+        arr[7] = odo.twist.twist.linear.y;
+        arr[8] = odo.twist.twist.linear.z;
+
+        serial.serial_send(arr, 1, sizeof(arr));
+        //std::cout<<"sizeof(arr)"<<sizeof(arr)<<std::endl;
+        sl.sleep();
+    }
+    urcu_memb_unregister_thread();
+}
+
+void calibration()
+{
+    Ten::Ten_relocation<pcl::PointXYZI> rel("/home/maple/study2/mapping/map.pcd");
+
+    std::cout << "Ten::Ten_relocation<pcl::PointXYZI> rel(/home/maple/study2/mapping/map.pcd);" << std::endl; 
+
+    Ten::XYZRPY xyzrpy = rel.get_transformation();
+
+    std::cout << "---------------------------" << std::endl; 
+    std::cout << "x: " << xyzrpy._xyz._x << std::endl;
+    std::cout << "y: " << xyzrpy._xyz._y << std::endl;
+    std::cout << "z: " << xyzrpy._xyz._z << std::endl;
+    std::cout << "roll: " << xyzrpy._rpy._roll << std::endl;
+    std::cout << "pitch: " << xyzrpy._rpy._pitch << std::endl;
+    std::cout << "yaw: " << xyzrpy._rpy._yaw << std::endl;
+
+
+    Ten::_COORDINATE_TRANSFORMATION_.set_world2toworld1(xyzrpy);
+}
+
+void serial_receiver()
+{
+    urcu_memb_register_thread();
+    Ten::Ten_serial& serial = Ten::Ten_serial::GetInstance();
+    uint8_t arr[1000] = {0};
+    ros::Rate sl(10);
+    while(ros::ok())
+    {
+        uint8_t frame_id = 0;
+        uint8_t length = 0;
+        if(serial.serial_read(arr, frame_id, length))
+        {
+            if(frame_id == 4)
+            {
+                calibration();
+            }
+        }
+        sl.sleep();
+    }
+    urcu_memb_unregister_thread();
+}
+
+
+
+
+
+// int main(int argc, char **argv)
+// {
+//     if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) {
+//     ros::console::notifyLoggerLevelsChanged();
+//     }
+
+//     ros::init(argc, argv, "merge_node");
+//     ros::NodeHandle nh;
+
+//     //test4();
+//     Ten::Ten_lidar::GetInstance("/home/maple/study2/merge_ws11/src/merge/src/livox_ros_driver2/config/MID360_config.json");
+//     Ten::ThreadPool pool(1);
+//     pool.enqueue(serial_send_lidar);
+
+//     laserMapping();
+//     Ten::Ten_lidar::GetInstance().~Ten_lidar();
+//     return 0;
+// }
+
+// int main(int argc, char **argv)
+// {
+//     if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) {
+//     ros::console::notifyLoggerLevelsChanged();
+//     }
+
+//     ros::init(argc, argv, "merge_node");
+//     ros::NodeHandle nh;
+
+//     test_relocation();
+//     return 0;
+// }
+
+
+// int main(int argc, char **argv)
+// {
+//     if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) {
+//     ros::console::notifyLoggerLevelsChanged();
+//     }
+
+//     ros::init(argc, argv, "merge_node");
+//     ros::NodeHandle nh;
+
+//     Ten::Ten_lidar::GetInstance("/home/maple/study2/merge_ws13/src/merge/src/livox_ros_driver2/config/MID360_config.json");
+//     // Ten::ThreadPool pool(1);
+//     // pool.enqueue(test_speed);
+//     laserMapping();
+//     Ten::Ten_lidar::GetInstance().~Ten_lidar();
+
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
+
+
