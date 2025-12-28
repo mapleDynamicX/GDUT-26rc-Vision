282a5a18e50090e033aff2ccfadd4e4c0ec2597e
mapleDynamicX <3697131063@qq.com>
2025-12-28 10:51:37 +0800

    17

---
+++ merge_ws17/src/merge/src/merge.cpp
@@ -0,0 +1,56 @@
+#include "merge_func.cpp"
+
+
+
+
+
+// int main(int argc, char **argv)
+// {
+//     if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) {
+//     ros::console::notifyLoggerLevelsChanged();
+//     }
+
+//     ros::init(argc, argv, "merge_node");
+//     ros::NodeHandle nh;
+
+//     //test4();
+//     Ten::Ten_lidar::GetInstance("/home/maple/study2/merge_ws11/src/merge/src/livox_ros_driver2/config/MID360_config.json");
+//     Ten::ThreadPool pool(1);
+//     pool.enqueue(serial_send_lidar);
+
+//     laserMapping();
+//     Ten::Ten_lidar::GetInstance().~Ten_lidar();
+//     return 0;
+// }
+
+// int main(int argc, char **argv)
+// {
+//     if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) {
+//     ros::console::notifyLoggerLevelsChanged();
+//     }
+
+//     ros::init(argc, argv, "merge_node");
+//     ros::NodeHandle nh;
+
+//     test5();
+//     return 0;
+// }
+
+
+int main(int argc, char **argv)
+{
+    if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) {
+    ros::console::notifyLoggerLevelsChanged();
+    }
+
+    ros::init(argc, argv, "merge_node");
+    ros::NodeHandle nh;
+
+    Ten::Ten_lidar::GetInstance("/home/maple/study2/merge_ws16/src/merge/src/livox_ros_driver2/config/MID360_config.json");
+    Ten::ThreadPool pool(1);
+    pool.enqueue(test_speed);
+    laserMapping();
+    Ten::Ten_lidar::GetInstance().~Ten_lidar();
+
+    return 0;
+}
